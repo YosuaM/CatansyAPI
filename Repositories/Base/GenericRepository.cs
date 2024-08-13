6fc4d547a93870b6daf8f7da5e37395c7b0371b6
kike <kikegumiel@gmail.com>
2024-08-13 20:43:31 +0200

    Switched to net6 version

--- Repositories/Base/GenericRepository.cs
+++ Repositories/Base/GenericRepository.cs
@@ -5,19 +5,26 @@ using CatansyAPI.Repositories.Base.Interfaces;
 
 namespace CatansyAPI.Repositories.Base;
 
-public class GenericRepository<T>(CatansyContext _catansyContext) : IGenericRepository<T> where T : class
+public class GenericRepository<T> : IGenericRepository<T> where T : class
 {
-    private DbSet<T> _entity => _catansyContext.Set<T>();
+    private readonly CatansyContext _catansyContext1;
+
+    public GenericRepository(CatansyContext _catansyContext)
+    {
+        _catansyContext1 = _catansyContext;
+    }
+
+    private DbSet<T> _entity => _catansyContext1.Set<T>();
 
     public void Add(T entity)
     {
         _entity.Add(entity);
-        _catansyContext.SaveChanges();
+        _catansyContext1.SaveChanges();
     }
 
     public async Task AddAsync(T entity)
     {
         await _entity.AddAsync(entity);
-        await _catansyContext.SaveChangesAsync();
+        await _catansyContext1.SaveChangesAsync();
     }
 }
--- Services/UsersService.cs
+++ Services/UsersService.cs
@@ -4,11 +4,18 @@ using CatansyAPI.Services.Interfaces;
 
 namespace CatansyAPI.Services;
 
-public class UsersService(IUsersReadOnlyRepository _usersRoRepository) : IUsersService
+public class UsersService : IUsersService
 {
+    private readonly IUsersReadOnlyRepository _usersRoRepository1;
+
+    public UsersService(IUsersReadOnlyRepository _usersRoRepository)
+    {
+        _usersRoRepository1 = _usersRoRepository;
+    }
+
     public async Task<UserDto?> SearchUserById(int id)
     {
-        var user = await _usersRoRepository.SearchUserById(id);
+        var user = await _usersRoRepository1.SearchUserById(id);
         return user?.ToDto();
     }
 }
